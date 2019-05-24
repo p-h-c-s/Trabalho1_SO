79dc898b12a780b82d8d21c7881d0224f9d786fb
Agnarok <abj@icomp.ufam.edu.br>
2019-05-23 23:51:14 -0400

    Versões com Struct

--- cpp_implementation/Questão 1/v2.cpp
+++ cpp_implementation/Questão 1/v2.cpp
@@ -7,62 +7,65 @@ using namespace std;
 
 int dinheiro=0;
 sem_t semaforo; //Semaforo unico pois nao temos preferencia entre Leitores e Escritores, pode gerar LEITURA SUJA!
+struct carrinho{
+    int itens;
+    int valor;
+};
+carrinho car;
 
-void * deposito(void *i){ //Escritora
+void * adiciona(void *i){ //Escritora
     sem_wait(&semaforo);
-    sleep(0.1);
-    int a =dinheiro;
-    cout << "(THREAD ESCRITORA) Antes do deposito:" << dinheiro<< endl;
-    a += *((int *)i);
-    sleep(0.5);
-    dinheiro = a;
-    cout << "(THREAD ESCRITORA) Depois do deposito:" << dinheiro<< endl;
+    cout << "(THREAD ESCRITORA) Antes de adicionar| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
+    car.itens++;
+    sleep(0.7);
+    car.valor += *((int *)i);
+    cout << "(THREAD ESCRITORA) Depois de adicionar| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
     sem_post(&semaforo);
 }
 
-void * saque(void *i){ //Escritora
+void * retira(void *i){ //Escritora
     sem_wait(&semaforo);
-    sleep(0.1);
-    int a =dinheiro;
-    cout << "(THREAD ESCRITORA) Antes do saque:" << dinheiro<< endl;
-    a -=*((int*)i);
-    sleep(0.5);
-    dinheiro = a;
-    cout << "(THREAD ESCRITORA) Depois do saque:" << dinheiro<< endl;
+    cout << "(THREAD ESCRITORA) Antes de retirar| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
+    car.itens--;
+    sleep(0.7);
+    car.valor -=*((int*)i);
+    cout << "(THREAD ESCRITORA) Depois de retirar| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
     sem_post(&semaforo);
 }
 
 void * consulta(void *i){ //Leitora
     sem_wait(&semaforo);
-    cout << "(THREAD LEITORA) Valor na conta:"<< dinheiro << endl;
+    cout << "(THREAD LEITORA) Carrinho| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
     sem_post(&semaforo);
 }
 
 int main(){
     sem_init(&semaforo,0,1);
-    int leitoras, num_deposito,num_saque;
-    cout << "Insira a quantidade de deposito, saque e leitoras: ";
-    cin >> num_deposito >> num_saque >> leitoras;
-    pthread_t threads[num_deposito+num_saque+leitoras];
-    int dep,saq;
-    cout << "Qual valor de deposito e saque: ";
-    cin >> dep >> saq;
-    for (int i = 0; i < num_deposito; i++){
-        pthread_create(&threads[i],NULL,deposito,&dep);
+    car.itens=0;
+    car.valor=0;
+    int leitoras, num_adiciona,num_retira;
+    cout << "Insira a quantidade de adiciona, retira e leitoras: ";
+    cin >> num_adiciona >> num_retira >> leitoras;
+    pthread_t threads[num_adiciona+num_retira+leitoras];
+    int add,rm;
+    cout << "Qual valor de adicionar e retira: ";
+    cin >> add >> rm;
+    for (int i = 0; i < num_adiciona; i++){
+        pthread_create(&threads[i],NULL,adiciona,&add);
     }
 
-    for (int i = num_deposito; i < num_deposito+num_saque; i++){
-        pthread_create(&threads[i],NULL,saque,&saq);
+    for (int i = num_adiciona; i < num_adiciona+num_retira; i++){
+        pthread_create(&threads[i],NULL,retira,&rm);
     }
     
-    for (int i = num_deposito+num_saque; i < num_deposito+num_saque+leitoras; i++){
+    for (int i = num_adiciona+num_retira; i < num_adiciona+num_retira+leitoras; i++){
         pthread_create(&threads[i],NULL,consulta,(void *)0);
     }
     
-    for (int i = 0; i < num_deposito+num_saque+leitoras; i++){
+    for (int i = 0; i < num_adiciona+num_retira+leitoras; i++){
         pthread_join(threads[i],NULL);
     }
     
-    cout << "\nVALOR FINAL:"<<dinheiro<<endl;
+    cout << "VALORES FINAIS| ITENS:" << car.itens<<" VALOR:"<< car.valor<<endl;
 
 }
